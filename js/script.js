5832906a2c821bbc1cba02de5f4fdb834f24a87b
sergiojanko <janko.sergio1998@gmail.com>
2021-07-08 17:34:02 +0200

    input-data-form

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/* 
+Quali campi inserire nel form:
+Una casella di testo per il form del passeggero
+Una casella numerica per i km
+Una tendina per selezionare la fascia d'età (Maggiorenne, Minorenne, Over65)
+NON E' NECESSARIA ALCUNA VALIDAZIONE
+Come calcolare  il prezzo:
+il prezzo del biglietto è definito in base ai km (0.21 € al km) 
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali,
+Cosa stampare sul biglietto:
+Nome passeggero
+Codice treno (inseriamo una stringa a caso, per ora)
+Numero carrozza (randomizziamo una carrozza da 1 a 12)
+Prezzo (stampato con due decimali)
+Tariffa applicata all'utente (Tariffa minori, Tariffa ordinaria, Tariffa Over65)
+*/
+
+
+//  * Recupero elementi HTML
+
+var nameField = document.getElementById("name-input");
+var kmsField = document.getElementById("kms-input");
+var ageField = document.getElementById("age-input");
+var btnGenerate = document.getElementById("generate");
+var btnReset = document.getElementById("reset");
+
+
+
+var nameOutput = document.getElementById("name-output");
+var discountOutput = document.getElementById("discount-output");
+var carOutput = document.getElementById("car-output");
+var codOutput = document.getElementById("cod-output");
+var priceOutput = document.getElementById("price-output");
+
+
